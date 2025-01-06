41e6ea4622098162a8c298e2e77407cb52fa372e
JulyYu <yuhaocan@sina.com>
2025-01-06 22:06:50 +0800

    update

---
+++ markdown2docx/md2docx.py
@@ -0,0 +1,124 @@
+import re
+from docx import Document
+from docx.shared import Inches
+import text_template as template
+import argparse
+import os
+
+def match_markdown_images(text):
+    # 正则表达式用于匹配 Markdown 中的![]() 图像标记
+    pattern = re.compile(r'!\[(.*?)\]\((.*?)\)')
+    matches = pattern.findall(text)
+    results = []
+    for match in matches:
+        # match 是一个元组，第一个元素是中括号内的内容，第二个元素是括号内的内容
+        results.append((match[0], match[1]))
+    return results
+
+def match_markdown_links(text):
+    # 正则表达式用于匹配 Markdown 中的 []() 链接
+    pattern = re.compile(r'\[(.*?)\]\((.*?)\)')
+    matches = pattern.findall(text)
+    results = []
+    for match in matches:
+        # match 是一个元组，第一个元素是中括号内的内容，第二个元素是括号内的内容
+        results.append((match[0], match[1]))
+    return results
+def is_url(url):
+    # 一个相对简单的 URL 正则表达式
+    pattern = re.compile(
+        r'^(https?|ftp)://'  # 协议部分，支持 http, https, ftp
+        r'([A-Za-z0-9.-]+)'  # 域名部分
+        r'(:[0-9]+)?'  # 端口部分，可选
+        r'(/[A-Za-z0-9_~:/?#[\]@!$&\'()*+,;=.%-]*)?'  # 路径部分，可选
+        r'(\?[A-Za-z0-9_~:/?#[\]@!$&\'()*+,;=.%-]*)?'  # 查询部分，可选
+        r'(#[A-Za-z0-9_~:/?#[\]@!$&\'()*+,;=.%-]*)?$'  # 片段部分，可选
+    )
+    return bool(pattern.match(url))
+
+def write_content(text):
+    document.add_paragraph(text)
+
+def write_pic(path):
+    document.add_picture(path)
+
+def write_end():
+    document.add_page_break()
+    document.save(output_name + '.docx')
+
+def read_markdown_file(file_path,write,writeEnd):
+    try:
+        with open(file_path, 'r') as file:
+            isArtiles = False
+            artContents = ''
+            for line_number, line in enumerate(file, start=1):
+                # 去除行尾的换行符
+                line = line.strip().rstrip('\n')
+                if len(line) > 0:
+                    if(line.startswith('## ')):
+                        title = line.replace('## ', "").strip()
+                        if(title == '📖好文章'):
+                            isArtiles = True
+                        else: 
+                            if isArtiles: 
+                                content = template.templateRedUl.substitute(ul = artContents)
+                                write(content.strip())
+                            isArtiles = False
+                        newline = template.templateRedTitleH2.substitute(h2 = title)
+                        write(newline.strip())
+                        # print(f"{newline.strip()}")
+                    else:
+                        # print(f" content ===> isArtiles {isArtiles} {line}")
+                        if isArtiles: 
+                            line = line.replace('* ', "").strip()
+                            results = match_markdown_links(line.strip())
+                            newline = template.templateRedLi.substitute(li = '📄' + results[0][0])
+                            # write(newline)
+                            artContents += newline.strip()
+                            url = template.templateRedUrl.substitute(url = results[0][1])
+                            artContents += url.strip()
+                            # print(f"isArtiles ===> {newline} ")
+                        else:
+                            if(line.startswith('**')):
+                                # print(f"title ===> {line} ")
+                                title = line.replace('*', "").strip()
+                                title = template.templateRedTitleH3.substitute(h3 = title)
+                                write(title.strip())
+                            elif is_url(line):
+                                # print(f"is_url ===> {line} ")
+                                url = template.templateRedUrl.substitute(url = line)
+                                write(url.strip())
+                            elif (line.strip().startswith('![')):
+                                imgPath = match_markdown_images(line);
+                                # print(f"imgPath ===> {imgPath[0][1]} {file_path.rsplit('/', 1)[0]}")
+                                write_pic(file_path.rsplit('/', 1)[0]+ "/" + imgPath[0][1])
+                            else:
+                                # print(f"line ===> {line} ")
+                                newLine = template.templateContent.substitute(content = line.strip())
+                                write(newLine.strip())
+
+            writeEnd()        
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def md2docx(file_path):
+    try:
+        document = Document()
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    # 在这里输入你要读取的文件的路径
+    file_path = '../Weekly/No21/No21.md'
+    parser = argparse.ArgumentParser(description='一个复杂的命令行参数示例')
+    parser.add_argument('-input_path',type=str, help='路径')
+    parser.add_argument('-output_name',type=str, help='名称')
+    args = parser.parse_args()
+    document = Document()
+    file_path = args.input_path
+    output_name = args.output_name
+    read_markdown_file(file_path,write_content,write_end)
+    #python3 md2docx.py -input_path ../Weekly/No21/No21.md -output_name 余小余周刊-第21期
---
+++ markdown2docx/text_template.py
@@ -0,0 +1,15 @@
+
+from string import Template
+
+templateRedTitleH2 = Template('''<h2 data-tool="mdnice编辑器" style="margin-top: 30px; padding: 0px; font-weight: bold; font-size: 22px; border-bottom: 2px solid rgb(89,89,89); margin-bottom: 30px; color: rgb(89,89,89);"><span class="prefix" style="display: none;"></span><span class="content" style="font-size: 22px; display: inline-block; border-bottom: 2px solid rgb(89,89,89);">$h2</span><span class="suffix"></span></h2>''')
+
+templateRedUl = Template('''<ul data-tool="mdnice编辑器" style="margin-top: 8px; margin-bottom: 8px; padding-left: 25px; color: black; list-style-type: disc;">$ul</ul>''')
+
+templateRedLi = Template('''<li><section style="margin-top: 5px; margin-bottom: 5px; line-height: 26px; text-align: left; color: rgb(1,1,1); font-weight: 500;">$li</section></li>''')
+
+templateRedTitleH3 = Template('''<h3 data-tool="mdnice编辑器" style="margin-top: 30px; margin-bottom: 15px; padding: 0px; font-weight: bold; font-size: 20px; color: rgb(89,89,89);"><span class="prefix" style="display: none;"></span><span class="content">$h3</span><span class="suffix" style="display: none;"></span></h3>''')
+
+
+templateRedUrl= Template('''<p data-tool="mdnice编辑器" style="font-size: 16px; padding-top: 8px; padding-bottom: 8px; margin: 0; line-height: 26px; color: rgb(89,89,89);"><strong style="font-weight: bold; color: rgb(71, 193, 168);">$url</strong></p>''')
+
+templateContent = Template('''<p data-tool="mdnice编辑器" style="font-size: 16px; padding-top: 8px; padding-bottom: 8px; margin: 0; line-height: 26px; color: black;">$content</p>''')

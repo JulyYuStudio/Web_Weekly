2e0b850d1cbd7787f297bcb45af1b30fdfd336e3
JulyYu <yuhaocan@weidian.com>
2025-04-14 16:29:49 +0800

    update

---
+++ markdown2docx/compress.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import os, sys
+import tinify
+
+
+def main():
+    parser = argparse.ArgumentParser(description='一个复杂的命令行参数示例')
+    parser.add_argument('-input_path',type=str, help='压缩目录')   
+     
+
+if __name__ == "__main__":
+   main()
